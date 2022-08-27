3753dfd276f0f7b3e8737339d68ae22cdfb4cd39
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-27 13:36:37 -0300

    Setup para conectar com planetscale realizado

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello')
+import express from 'express'
+
+const App = express()
+
+App.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+App.listen(PORT, ()=> {
+    console.log(`App running on http://localhost:${PORT}`)
+})
+
+console.log('Hello world')

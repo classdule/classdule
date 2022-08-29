ae7c0917134dfb476d2c302ef957ae0c4807967f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-29 10:31:06 -0300

    Alterações para preparar os testes

--- server/index.ts
+++ server/index.ts
@@ -8,18 +8,24 @@ import { authRoutes } from './src/routes/auth.routes'
 import { userRoutes } from './src/routes/user.routes'
 import { beltRoutes } from './src/routes/belt.routes'
 
-const App = express()
+export function createServer(){
+    const App = express()
 
-App.use(
-    express.json(),
-    mainRoutes,
-    authRoutes,
-    userRoutes,
-    beltRoutes
-)
+    App.use(
+        express.json(),
+        mainRoutes,
+        authRoutes,
+        userRoutes,
+        beltRoutes
+    )
+    return App
+}
+
 
 const PORT = process.env.PORT || 3000
 
-App.listen(PORT, ()=> {
+const server = createServer()
+
+server.listen(PORT, ()=> {
     console.log(`App running on http://localhost:${PORT}`)
 })
---
+++ server/test/auth.routes.test.ts
@@ -0,0 +1,6 @@
+describe('Testes', ()=> {
+    it('Should sum', () => {
+        expect(4).toBe(4)
+    })
+})
+

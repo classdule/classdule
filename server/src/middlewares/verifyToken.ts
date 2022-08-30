ecf9548b4bf2da6435f236545e201256571f69e7
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-30 11:21:52 -0300

    Atualizada resposta do servidor para verificação de token

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -4,16 +4,12 @@ import jwt, { Secret } from "jsonwebtoken";
 export function verifyToken(req:Request, res:Response, next:NextFunction){
     let token = req.headers['x-access-token']
     if(!token){
-        res.status(403).json({
-            message:'No token provided'
-        })
+        res.status(401).json({})
     }
     const tokenSecret = process.env.JWT_TOKEN_SECRET as Secret
     jwt.verify(token as string, tokenSecret, (err, decoded) => {
         if(err){
-            return res.status(403).json({
-                message:'Acesso não autorizado'
-            })
+            return res.status(403).json({})
         }
         req.body.user = decoded
         next()

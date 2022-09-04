4c240bfad55acae23cc20374ca063ab5ece96da3
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-09-04 16:53:02 -0300

    Implementados testes de acesso

--- server/test/user.routes.test.ts
+++ server/test/user.routes.test.ts
@@ -1,11 +1,23 @@
 import request from 'supertest'
 import { createServer } from '../src/app'
 
+const invalidAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
 describe('Endpoint tests', ()=> {
     it('Should return a list of users', async ()=> {
         const response = await request(createServer())
             .get('/users')
             .expect(200)
         expect(response.body.length > 0).toBe(true)
+    })
+
+    it('Should not give access since user is not authenticated', async () => {
+        await request(createServer()).post('/user/changeUsername').expect(401)
+    })
+    it('Should not give access since user authentication is invalid', async () => {
+        await request(createServer())
+            .post('/user/changeUsername')
+            .set('Cookie', [`access_token=${invalidAccessToken}`])
+            .expect(403)
     })
 })

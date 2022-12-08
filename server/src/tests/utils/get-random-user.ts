0f1be11bd0bb35c513222bb3ea00e114b17166bf
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-08 15:50:44 -0300

    Corrigida função simples de teste

--- server/src/tests/utils/get-random-user.ts
+++ server/src/tests/utils/get-random-user.ts
@@ -1,14 +1,11 @@
-import { subDays } from "date-fns";
-import {v4 as uuid} from 'uuid';
 import { User } from "../../entities/user";
 import { getPastDate } from "./get-past-date";
 
-export function getRandomUser(): User{
-    return new User({
-        birthDay: getPastDate(),
-        name: 'John Doe',
-        academyIds: [uuid(), uuid()],
-        password: 'password',
-        email: 'email@email.com',
-    });
+export function getRandomUser(): User {
+  return new User({
+    birthDay: getPastDate(),
+    name: "John Doe",
+    password: "password",
+    email: "email@email.com",
+  });
 }

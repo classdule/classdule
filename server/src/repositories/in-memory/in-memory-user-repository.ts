e9d63b5fb998c09f0bcb92d0f60a63f10a4a1f9d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-14 16:14:56 -0300

    (style) Pequena re-indentação

--- server/src/repositories/in-memory/in-memory-user-repository.ts
+++ server/src/repositories/in-memory/in-memory-user-repository.ts
@@ -2,39 +2,37 @@ import { User } from "../../entities/user";
 import { UserRepositoryBase } from "../user-repository";
 
 export class InMemoryUserRepository implements UserRepositoryBase {
-    users: User[] = []
-    async create (user: User){
-        this.users.push(user);
-        return user;
-    }
-    async delete (userId: string){
-        const deleteUser = this.users.find(user => user.id === userId) || null
-        this.users = this.users.filter(user => user.id !== userId)
-        return deleteUser
-        
-    }
+  users: User[] = [];
+  async create(user: User) {
+    this.users.push(user);
+    return user;
+  }
+  async delete(userId: string) {
+    const deleteUser = this.users.find((user) => user.id === userId) || null;
+    this.users = this.users.filter((user) => user.id !== userId);
+    return deleteUser;
+  }
 
-    async changeUserName (userId: string, username: string) {
-        const userIndex = this.users.findIndex(user => user.id === userId)
-        this.users[userIndex].name = username;
-        return this.users[userIndex]
-    }
+  async changeUserName(userId: string, username: string) {
+    const userIndex = this.users.findIndex((user) => user.id === userId);
+    this.users[userIndex].name = username;
+    return this.users[userIndex];
+  }
 
-    async findByEmail (email: string){
-        const user = this.users.find(user => user.email === email);
-        if(!!user){
-            return user;
-        }
-        return null;
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email);
+    if (!!user) {
+      return user;
     }
-    async queryByName(name:string) {
-        return this.users.filter(user => name.includes(user.name));
-    }
-    async findAll(){
-        return this.users;
-    }
-    async findById(userId: string){
-        return this.users.find(user => user.id === userId) ?? null;
-    }
-
+    return null;
+  }
+  async queryByName(name: string) {
+    return this.users.filter((user) => name.includes(user.name));
+  }
+  async findAll() {
+    return this.users;
+  }
+  async findById(userId: string) {
+    return this.users.find((user) => user.id === userId) ?? null;
+  }
 }

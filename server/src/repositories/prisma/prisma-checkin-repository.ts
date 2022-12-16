8ef4554350db307bcd58a4669f5b5d725d570e10
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-15 21:54:02 -0300

    (refactor) Entity mappers aplicados no repositório prisma check-in

--- server/src/repositories/prisma/prisma-checkin-repository.ts
+++ server/src/repositories/prisma/prisma-checkin-repository.ts
@@ -1,106 +1,65 @@
 import { isSameDay } from "date-fns";
 import { prismaClient } from "../../database/prisma";
 import { Checkin } from "../../entities/checkin";
+import { CheckinPrismaMapper } from "../../mappers/prisma/checkin-prisma-mapper";
 
 import { CheckinRepository } from "../checkin-repository";
 
 export class PrismaCheckinRepository implements CheckinRepository {
-    async create(checkin: Checkin){
-        const createdCheckin = await prismaClient.checkin.create({
-            data: {
-                classroomId: checkin.classroomId,
-                userId: checkin.userId
-            }
-        })
-        return new Checkin({
-            classroomId: createdCheckin.classroomId,
-            userId: createdCheckin.userId,
-            createdAt: createdCheckin.createdAt,
-            verified: createdCheckin.verified
-        }, createdCheckin.id)
-    }
-    async delete(checkinId: string){
-        const deletedCheckin = await prismaClient.checkin.delete({
-            where: {
-                id: checkinId
-            }
-        })
-        if(!deletedCheckin){
-            return null
-        }
-        return new Checkin({
-            classroomId: deletedCheckin.classroomId,
-            userId: deletedCheckin.userId,
-            createdAt: deletedCheckin.createdAt,
-            verified: deletedCheckin.verified 
-        }, deletedCheckin.id)
-    }
-    async verify(checkinId: string, verify: boolean){
-        const verifiedCheckin = await prismaClient.checkin.update({
-            where: {
-                id: checkinId
-            },
-            data: {
-                verified: verify
-            }
-        })
-        if(!verifiedCheckin){
-            return null
-        }
-        return new Checkin({
-            classroomId: verifiedCheckin.classroomId,
-            userId: verifiedCheckin.userId,
-            createdAt: verifiedCheckin.createdAt,
-            verified: verifiedCheckin.verified
-        }, verifiedCheckin.id)
-    }
+  async create(checkin: Checkin) {
+    await prismaClient.checkin.create({
+      data: {
+        classroomId: checkin.classroomId,
+        userId: checkin.userId,
+      },
+    });
+  }
+  async delete(checkinId: string) {
+    await prismaClient.checkin.delete({
+      where: {
+        id: checkinId,
+      },
+    });
+  }
+  async verify(checkinId: string, verify: boolean) {
+    await prismaClient.checkin.update({
+      where: {
+        id: checkinId,
+      },
+      data: {
+        verified: verify,
+      },
+    });
+  }
 
-    async findByUserId(userId: string){
-        const queryResult = await prismaClient.checkin.findMany({
-            where: {
-                userId: userId
-            }
-        })
-        return queryResult.map(checkin => {
-            return new Checkin({
-                classroomId: checkin.classroomId,
-                userId: checkin.userId,
-                createdAt: checkin.createdAt,
-                verified: checkin.verified
-            })
-        })
-    }
-    async findByDate(date: Date, userId: string){
-        const queryResult = (await prismaClient.checkin.findMany({
-            where: {
-                userId: userId
-            }
-        }))
-            .filter(checkin => isSameDay(checkin.createdAt, date));
+  async findByUserId(userId: string) {
+    const queryResult = await prismaClient.checkin.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+    return queryResult.map(CheckinPrismaMapper.toDomain);
+  }
+  async findByDate(date: Date, userId: string) {
+    const queryResult = (
+      await prismaClient.checkin.findMany({
+        where: {
+          userId: userId,
+        },
+      })
+    ).filter((checkin) => isSameDay(checkin.createdAt, date));
 
-        return queryResult.map(checkin => {
-            return new Checkin({
-                classroomId: checkin.classroomId,
-                userId: checkin.userId,
-                createdAt: checkin.createdAt,
-                verified: checkin.verified
-            })
-        })
+    return queryResult.map(CheckinPrismaMapper.toDomain);
+  }
+  async findById(checkinId: string) {
+    const foundCheckin = await prismaClient.checkin.findUnique({
+      where: {
+        id: checkinId,
+      },
+    });
+    if (!foundCheckin) {
+      return null;
     }
-    async findById(checkinId: string){
-        const foundCheckin = await prismaClient.checkin.findUnique({
-            where: {
-                id: checkinId
-            }
-        })
-        if(!foundCheckin){
-            return null
-        }
-        return new Checkin({
-            classroomId: foundCheckin.classroomId,
-            userId: foundCheckin.userId,
-            createdAt: foundCheckin.createdAt,
-            verified: foundCheckin.verified
-        });
-    }
+    return CheckinPrismaMapper.toDomain(foundCheckin);
+  }
 }

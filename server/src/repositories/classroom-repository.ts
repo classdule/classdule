64be6f9e588aece4f35effc65ed8439cad64da7d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-15 21:49:57 -0300

    (refactor) Entity mapper aplicado ao repositório prisma classroom

--- server/src/repositories/classroom-repository.ts
+++ server/src/repositories/classroom-repository.ts
@@ -3,8 +3,8 @@ import { Day } from "date-fns";
 import { Classroom } from "../entities/classroom";
 
 export interface ClassroomRepository {
-  create: (classroom: Classroom) => Promise<Classroom>;
-  delete: (classroomId: string) => Promise<Classroom | null>;
+  create: (classroom: Classroom) => Promise<void>;
+  delete: (classroomId: string) => Promise<void>;
   findOverlappingDateClassroom: (
     start: Date,
     end: Date,
--- server/src/repositories/prisma/prisma-classroom-repository.ts
+++ server/src/repositories/prisma/prisma-classroom-repository.ts
@@ -4,11 +4,12 @@ import { intersection } from "lodash";
 import { prismaClient } from "../../database/prisma";
 
 import { Classroom } from "../../entities/classroom";
+import { ClassroomPrismaMapper } from "../../mappers/prisma/classroom-prisma-mapper";
 import { ClassroomRepository } from "../classroom-repository";
 
 export class PrismaClassroomRepository implements ClassroomRepository {
   async create(classroom: Classroom) {
-    const queryResult = await prismaClient.classroom.create({
+    await prismaClient.classroom.create({
       data: {
         startsAt: classroom.startsAt,
         endsAt: classroom.endsAt,
@@ -35,57 +36,18 @@ export class PrismaClassroomRepository implements ClassroomRepository {
             };
           }),
         },
-        content: {
-          connectOrCreate: classroom.content.map((cont) => {
-            return {
-              where: {
-                content: cont,
-              },
-              create: {
-                content: cont,
-              },
-            };
-          }),
-        },
+        content: classroom.content,
       },
       include: {
         weekdays: true,
-        content: true,
       },
     });
-    return new Classroom(
-      {
-        groupId: queryResult.groupId,
-        educatorId: queryResult.educatorId,
-        endsAt: queryResult.endsAt,
-        startsAt: queryResult.startsAt,
-        type: queryResult.type,
-        weekdays: queryResult.weekdays.map((weekday) => weekday.weekday as Day),
-        content: queryResult.content.map((cont) => cont.content),
-      },
-      queryResult.id
-    );
   }
   async delete(classroomId: string) {
-    const deletedClassroom = await prismaClient.classroom.delete({
+    await prismaClient.classroom.delete({
       where: {
         id: classroomId,
       },
-      include: {
-        weekdays: true,
-        content: true,
-      },
-    });
-    return new Classroom({
-      groupId: deletedClassroom.groupId,
-      educatorId: deletedClassroom.educatorId,
-      endsAt: deletedClassroom.endsAt,
-      startsAt: deletedClassroom.startsAt,
-      type: deletedClassroom.type,
-      weekdays: deletedClassroom.weekdays.map(
-        (weekday) => weekday.weekday as Day
-      ),
-      content: deletedClassroom.content.map((cont) => cont.content),
     });
   }
   async findOverlappingDateClassroom(
@@ -101,7 +63,6 @@ export class PrismaClassroomRepository implements ClassroomRepository {
         },
         include: {
           weekdays: true,
-          content: true,
         },
       })
     ).filter(
@@ -126,20 +87,7 @@ export class PrismaClassroomRepository implements ClassroomRepository {
     if (!overlappingClassroom) {
       return null;
     }
-    return new Classroom(
-      {
-        groupId: overlappingClassroom.groupId,
-        educatorId: overlappingClassroom.educatorId,
-        endsAt: overlappingClassroom.endsAt,
-        startsAt: overlappingClassroom.startsAt,
-        type: overlappingClassroom.type,
-        weekdays: overlappingClassroom.weekdays.map(
-          (weekday) => weekday.weekday as Day
-        ),
-        content: overlappingClassroom.content.map((cont) => cont.content),
-      },
-      overlappingClassroom.id
-    );
+    return ClassroomPrismaMapper.toDomain(overlappingClassroom);
   }
   async findById(classroomId: string) {
     const foundClassroom = await prismaClient.classroom.findUnique({
@@ -148,23 +96,12 @@ export class PrismaClassroomRepository implements ClassroomRepository {
       },
       include: {
         weekdays: true,
-        content: true,
       },
     });
     if (!foundClassroom) {
       return null;
     }
-    return new Classroom({
-      groupId: foundClassroom.groupId,
-      educatorId: foundClassroom.educatorId,
-      endsAt: foundClassroom.endsAt,
-      startsAt: foundClassroom.startsAt,
-      type: foundClassroom.type,
-      weekdays: foundClassroom.weekdays.map(
-        (weekday) => weekday.weekday as Day
-      ),
-      content: foundClassroom.content.map((cont) => cont.content),
-    });
+    return ClassroomPrismaMapper.toDomain(foundClassroom);
   }
   async findByGroup(groupId: string) {
     const academyClassrooms = await prismaClient.classroom.findMany({
@@ -173,22 +110,8 @@ export class PrismaClassroomRepository implements ClassroomRepository {
       },
       include: {
         weekdays: true,
-        content: true,
       },
     });
-    return academyClassrooms.map((classroom) => {
-      return new Classroom(
-        {
-          groupId: classroom.groupId,
-          educatorId: classroom.educatorId,
-          endsAt: classroom.endsAt,
-          startsAt: classroom.startsAt,
-          type: classroom.type,
-          weekdays: classroom.weekdays.map((weekday) => weekday.weekday as Day),
-          content: classroom.content.map((cont) => cont.content),
-        },
-        classroom.id
-      );
-    });
+    return academyClassrooms.map(ClassroomPrismaMapper.toDomain);
   }
 }
